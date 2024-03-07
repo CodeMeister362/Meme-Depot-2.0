66f303297accc88cbcb8ed55b97050ee09c53408
nathan chamberlain <chamberlainuxdesign@gmail.com>
2024-03-07 15:58:49 -0800

    Adds fields and functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-import Meme from './components/Meme';
+import Main from './components/Main';
 
 function App() {
   return (
     <div>
       <Header />
-      <Meme />
+      <Main />
     </div>
   );
 }
